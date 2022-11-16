6ce3bc788012094e2baa279472ef9b6b54ac68a1
emsiqh <emsiqh2k1@gmail.com>
2022-11-16 23:23:11 +0700

    add some css hover and fix some bugs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ styles }) => {
     return (
-        <button type='button' className={`capitalize py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+        <button type='button' className={`btn capitalize py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
             get started
         </button>
     )
